7aaccd13a7fa58b9cc0cc00814c4ff01b3135cdb
guihi1 <gui.hikio@gmail.com>
2022-09-09 19:38:57 -0300

    Add new variables and new eventListener

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 const library = document.getElementById("library");
+const newBook = document.querySelector(".add-button");
+const form = document.getElementById("form");
+const mask = document.getElementById("page-mask");
 
 function book(title, author, pages, read) {
     this.title = title;
@@ -8,8 +11,8 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
-// book.prototype.info = function() {
-//     return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+// book.prototype.read = function() {
+//     
 // }
 
 function addBookToLibrary(obj) {
@@ -18,7 +21,20 @@ function addBookToLibrary(obj) {
 
 const book1 = new book("hobbit", "JJR", "225", true);
 addBookToLibrary(book1);
+const book2 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book2);
+const book3 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book3);
+const book4 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book4);
+const book5 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book5);
+const book6 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book6);
+const book7 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book7);
 console.log(myLibrary);
+displayBooks();
 
 function displayBooks() {
     for(let i = 0; i < myLibrary.length; i++) {
@@ -30,5 +46,17 @@ function displayBooks() {
             p.textContent = `${myLibrary[i][book]}`;
             div.appendChild(p);
         }
+        const remove = document.createElement("button");
+        remove.textContent = "Remove"
+        div.appendChild(remove);
     }
+}
+
+newBook.addEventListener("click", () => {
+    mask.style.visibility = "visible";
+    form.style.visibility = "visible";
+});
+
+function closeForm() {
+    form.style.visibility = "hidden";
 }
