fa016fa190a07f5db25ce8a14d37807ee2c53977
guihi1 <gui.hikio@gmail.com>
2022-09-09 13:47:00 -0300

    Create book object constructor

---
+++ index.js
@@ -0,0 +1,10 @@
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+book.prototype.info = function() {
+    return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+}
