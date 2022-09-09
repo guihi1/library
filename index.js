fa28ca4595e14087339ab88f7011efc09dd1f3a8
guihi1 <gui.hikio@gmail.com>
2022-09-09 15:23:28 -0300

    Create function displayBooks

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let myLibrary = [];
+const library = document.getElementById("library");
+
 function book(title, author, pages, read) {
     this.title = title;
     this.author = author;
@@ -5,6 +8,27 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
-book.prototype.info = function() {
-    return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+// book.prototype.info = function() {
+//     return this.title + " by " + this.author + ", " + this.pages + " pages, " + this.read;
+// }
+
+function addBookToLibrary(obj) {
+    myLibrary.push(obj);
+}
+
+const book1 = new book("hobbit", "JJR", "225", true);
+addBookToLibrary(book1);
+console.log(myLibrary);
+
+function displayBooks() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        const div = document.createElement("div");
+        div.classList.add("book");
+        library.appendChild(div);
+        for(const book in myLibrary[i]) {
+            const p = document.createElement("p");
+            p.textContent = `${myLibrary[i][book]}`;
+            div.appendChild(p);
+        }
+    }
 }
