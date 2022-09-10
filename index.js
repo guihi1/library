d3a2237f39652b09b5d37f2ce86b5a148a0a8869
guihi1 <gui.hikio@gmail.com>
2022-09-10 19:54:38 -0300

    Fix remove button

--- index.js
+++ index.js
@@ -11,44 +11,67 @@ function book(title, author, pages, read) {
     this.read = read;
 }
 
-// book.prototype.read = function() {
-//     
-// }
+book.prototype.readStatus = () => {
+    if(this.read){
+        this.read = false;
+        
+    } else {
+        this.read = true;
+    }
+    console.log(this.read);
+}
 
 function addBookToLibrary(obj) {
     myLibrary.push(obj);
 }
 
-const book1 = new book("hobbit", "JJR", "225", true);
+const book1 = new book("hobbit", "JJR", "1", true);
 addBookToLibrary(book1);
-const book2 = new book("hobbit", "JJR", "225", true);
+const book2 = new book("hofds", "JJR", "2", false);
 addBookToLibrary(book2);
-const book3 = new book("hobbit", "JJR", "225", true);
+const book3 = new book("dfsft", "JJR", "3", true);
 addBookToLibrary(book3);
-const book4 = new book("hobbit", "JJR", "225", true);
+const book4 = new book("fdsfs", "JJR", "4", false);
 addBookToLibrary(book4);
-const book5 = new book("hobbit", "JJR", "225", true);
+const book5 = new book("fdsfsdf", "JJR", "5", true);
 addBookToLibrary(book5);
-const book6 = new book("hobbit", "JJR", "225", true);
+const book6 = new book("fsdfsd", "JJR", "6", false);
 addBookToLibrary(book6);
-const book7 = new book("hobbit", "JJR", "225", true);
+const book7 = new book("hfdsf", "JJR", "7", true);
 addBookToLibrary(book7);
 console.log(myLibrary);
 displayBooks();
 
+
+
 function displayBooks() {
+    while(library.firstChild){
+        library.removeChild(library.lastChild);
+    }
     for(let i = 0; i < myLibrary.length; i++) {
         const div = document.createElement("div");
         div.classList.add("book");
         library.appendChild(div);
         for(const book in myLibrary[i]) {
-            const p = document.createElement("p");
-            p.textContent = `${myLibrary[i][book]}`;
-            div.appendChild(p);
+            if(myLibrary[i].hasOwnProperty(book)) {
+                const p = document.createElement("p");
+                p.textContent = `${myLibrary[i][book]}`;
+                div.appendChild(p);
+            }
         }
+        const reading = document.createElement("button");
+        reading.textContent="Change read status";
+        reading.classList.add("reading");
+        div.appendChild(reading);
+        reading.addEventListener("click", myLibrary[i]["read"].readStatus);
         const remove = document.createElement("button");
         remove.textContent = "Remove"
+        remove.classList.add("remove");
         div.appendChild(remove);
+        remove.addEventListener("click", () => {
+            myLibrary.splice(i, 1);
+            displayBooks();
+        });
     }
 }
 
@@ -56,7 +79,3 @@ newBook.addEventListener("click", () => {
     mask.style.visibility = "visible";
     form.style.visibility = "visible";
 });
-
-function closeForm() {
-    form.style.visibility = "hidden";
-}
